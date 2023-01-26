6f2f73a7fb0b4ac8e58c12f4b6457c5da4d7c58e
Tyler Tracy <tylertracy1999@gmail.com>
2023-01-26 16:58:32 -0600

    Complexity theory assignment 1

---
+++ computational_complexity/assignment_1/package.sh
@@ -0,0 +1,3 @@
+cp out/report.pdf Assignment1.pdf
+zip -r Assignment1.zip Assignment1.pdf problem1.jff problem2.jff turing-machine-sim.py input.txt run.sh
+rm Assignment1.pdf
---
+++ computational_complexity/assignment_1/run.sh
@@ -0,0 +1,2 @@
+encodedInput="$(<problem2.jff)💻$(<input.txt)"
+echo "$encodedInput" | python3 turing-machine-sim.py
